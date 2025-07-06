40da1b4fb9b8eb3f117941339215b4ee29008073
jannatcodingal <jannatrehman007@gmail.com>
2025-07-06 19:14:07 +0500

    project

--- project.py
+++ project.py
@@ -2,10 +2,14 @@ import pygame
 pygame.init()
 screen=pygame.display.set_mode((640,480))
 ss=pygame.display.set_caption("my first game screen")
-done=False
+pygame.draw.rect(screen, (125,125,125), pygame.Rect(200,200,120, 120))
+text_font=pygame.font.Font(None, 36)
+text_surface=text_font.render("Hello, Pygame!", True, (255, 255, 255))
+screen.blit(text_surface, (200, 200))
+pygame.display.flip()
+done = False
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done=True
-    pygame.draw.rect(screen, (125,125,125), pygame.Rect(180,180,170,170))
-    pygame.display.flip()
+            done = True
+pygame.quit()
